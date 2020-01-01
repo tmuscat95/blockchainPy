957979884102de6faff12b886aa109a65b84512b
Timothy Muscat <tim@Timothys-MBP.lan>
2020-01-01 16:45:50 +0100

    fixed indentation in functions. added verification of all open transactions function

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,12 @@
+import random
+import sys
+
 blockchain = []
 wallets = []
 open_transactions=[]
+origin_wallet = 0
+
+MINING_REWARD = 5
 
 def blockHash(block):
     x = str([block[key] for key in block])
@@ -40,7 +46,9 @@ def verifyChain():
             if (blockHash(blockchain[block.index-1]) != block.prevBlockHash):
                 verifiedChain = False
                 break
+
     return verifiedChain
+
 
 def getBalance(walletID):
     balance = 0
@@ -50,13 +58,50 @@ def getBalance(walletID):
                 balance -= transaction.amount
             elif(transaction.receiver==walletID):
                 balance+= transaction.amount
-            return balance
+        
+    return balance
 
+def verify_open_transactions():
+    return all([verify_transaction(t.sender,t.amount) for t in open_transactions])
+
+
+def verify_transaction(sender_wallet,tx_amount):
+        #determines whether sender has enough balance to make transaction; takes into account open transactions as well to prevent double spending
+        balance = getBalance(sender_wallet)
+        for transaction in open_transactions:
+            if(transaction.sender==sender_wallet):
+                balance -= transaction.amount
+        
+        if(balance - tx_amount >= 0):
+            return True
+        else:
+            return False
+
+def addTransaction(sender_wallet,receiver_wallet,tx_amount):
+    if verify_transaction(sender_wallet,tx_amount):
+        open_transactions.append(Transaction(sender=sender_wallet,receiver=receiver_wallet,amount=tx_amount))
+        return True
+    else:
+        return False
+
+def mineCoin():
+    global open_transactions,blockchain
+    """assigns mining reward for block to miner wallet(random for now), is simply added to list of open transactions with origin wallet as sender
+        to be processed with other open transactions when new block is created.
+    """
+    miner = random.randint(1,len(wallets)-1)
+    addTransaction(sender_wallet=origin_wallet,receiver_wallet=miner,tx_amount=MINING_REWARD)
+    newblock = Block()
+    blockchain.append(newblock,open_transactions)
+    open_transactions = []
 
 def main():
-    global blockchain,open_transactions,wallets
+    global blockchain,open_transactions,wallets,origin_wallet
+
     genesis_block = Block()
     blockchain += [genesis_block]
+    wallets.append(random.randint(0,sys.maxint)) #origin wallet
+    origin_wallet = wallets[0]
 
     waiting_for_input = True
     while waiting_for_input:
@@ -74,14 +119,24 @@ def main():
             recv_wallet = input("Enter Recipient Wallet:")
             tx_amount = input("Enter Amount:")
             # Add the transaction amount to the open_transactions
-            open_transactions.append(Transaction(sender=sender_wallet,receiver=recv_wallet,amount=tx_amount))
+            if not addTransaction(sender_wallet=sender_wallet,recv_wallet=recv_wallet,tx_amount=tx_amount):
+                print("Insufficient Balance.")
+            else:
+                print("Success.")
+
         elif user_choice == '2':
             print_blockchain_elements()
         elif user_choice == '3':
-            blockchain.append(Block(),open_transactions)
-            open_transactions = []
+            mineCoin()
         elif user_choice == '4':
-            wallets.append(input("Enter Wallet ID:"))
+            wallets.append(input("Enter Wallet ID: "))
+        elif user_choice == '5':
+            print(getBalance(input("Enter Wallet ID: ")))
+        elif user_choice == '6':
+            if(verify_open_transactions()):
+                print("Open Transactions OK")
+            else:
+                print("Invalid transactions.")
         elif user_choice == 'h':
             # Make sure that you don't try to "hack" the blockchain if it's empty
             if len(blockchain) >= 1:
@@ -101,6 +156,7 @@ def main():
 
 
     print('Done!')
+
 
 
 if(__name__=='__main__'):
